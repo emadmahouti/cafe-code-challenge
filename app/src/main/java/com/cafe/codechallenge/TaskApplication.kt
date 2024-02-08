1c8d08c0b101b3a06d90940cfc75d64074fd4243
Emad <emad.mahouti@gmail.com>
2024-02-08 17:44:55 +0330

    setup task application

--- app/src/main/java/com/cafe/codechallenge/TaskApplication.kt
+++ app/src/main/java/com/cafe/codechallenge/TaskApplication.kt
@@ -1,7 +1,10 @@
 package com.cafe.codechallenge
 
 import android.app.Application
+import com.cafe.codechallenge.util.providers.StyleProvider
 import com.pixy.codebase.Codebase
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 /**
  * Created by emadmahouti on 2/8/24
@@ -9,5 +12,14 @@ import com.pixy.codebase.Codebase
 class TaskApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Codebase.init(StyleProvider.Default())
+        startDI()
+    }
+
+    private fun startDI() {
+        startKoin {
+            androidContext(this@TaskApplication)
+        }
     }
 }
