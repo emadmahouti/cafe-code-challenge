24a77d19b35564e50b9d02f96b7bfbeedb997f41
Emad <emad.mahouti@gmail.com>
2024-02-08 17:30:39 +0330

    folder structure

---
+++ app/src/main/java/com/cafe/codechallenge/TaskApplication.kt
@@ -0,0 +1,13 @@
+package com.cafe.codechallenge
+
+import android.app.Application
+import com.pixy.codebase.Codebase
+
+/**
+ * Created by emadmahouti on 2/8/24
+ */
+class TaskApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
