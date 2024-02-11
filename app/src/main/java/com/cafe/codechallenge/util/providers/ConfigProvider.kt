0c1cb52fe38e0dca8335ce70bb9ea1d83001abcc
Emad <emad.mahouti@gmail.com>
2024-02-11 21:04:57 +0330

    add icon

--- app/src/main/java/com/cafe/codechallenge/util/providers/ConfigProvider.kt
+++ app/src/main/java/com/cafe/codechallenge/util/providers/ConfigProvider.kt
@@ -5,6 +5,6 @@ package com.cafe.codechallenge.util.providers
  */
 class ConfigProvider {
     val base_url = "https://api.themoviedb.org/3/"
-    val image_base_url = "https://image.tmdb.org/t/p/original/"
+    val image_base_url = "https://image.tmdb.org/t/p/w500/"
     val user_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzUwOGMwZWY5OWVmYThmZDUwMTkzNGYxM2ZkNDgzOCIsInN1YiI6IjYyNTgwNzM5YWZlMjI0MDA5ZDgyZTdjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VKDe4yaxy9ooBrIjGWZlR0pOGp6CWkswggWiR6c2gSM"
 }
